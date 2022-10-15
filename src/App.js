9b943b540518481afd1fa3a5a156db24692b34ad
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-10-15 19:38:15 -0400

    install material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button variant="contained">Learn React</Button>
         </a>
       </header>
     </div>
