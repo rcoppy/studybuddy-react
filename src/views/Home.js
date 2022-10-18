882f6b3e3b479abcb1bc0857ea08be242c5900f7
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-10-17 21:03:09 -0400

    reactive homepage to group status (hide/show nav buttons)

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,39 @@
 import CreateGroupFab from '../widgets/CreateGroupFab';
 import StudentProfileModel from '../lib/StudentProfileModel';
 import * as React from 'react';
-import { Typography } from '@mui/material';
+import { Button, Container, Divider, Typography } from '@mui/material';
 import { GlobalContext } from '../lib/GlobalContext';
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom';
+import GroupsIcon from '@mui/icons-material/Groups';
 
+function GalleryLinks({shouldShow}) {
+    if (!shouldShow) return <Typography variant="p" sx={{ mb: 2 }}><em>You're not currently looking for groups.</em></Typography>;
+
+    return <>
+        <Button variant="contained" sx={{ mb: 2 }} startIcon={<SearchIcon />} component={Link} to="/gallery/groups">
+            Explore groups
+        </Button>
+        <Button variant="contained" sx={{ mb: 2 }} startIcon={<SearchIcon />} component={Link} to="/gallery/students">
+            Explore students
+        </Button>
+        <Divider sx={{ mb: 2 }} />
+    </>;
+}
 
 function Home() {
     return (
         <>
             <GlobalContext.Consumer>
-                {({myProfile}) => <>
-                    <Typography variant="h4">You are currently <em>{myProfile.amLookingForGroups ? "looking" : "not looking"}</em> for groups.</Typography>
+                {({ myProfile }) => <>
                     <CreateGroupFab />
+                    {/* <Typography variant="h4">You are currently <em>{myProfile.amLookingForGroups ? "looking" : "not looking"}</em> for groups.</Typography> */}
+
+                    <Container maxWidth="md" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <GalleryLinks shouldShow={myProfile.amLookingForGroups} />
+                        <GroupsIcon />
+                        <Typography variant="p"><em>Your active groups will appear here.</em></Typography>
+                    </Container>
                 </>}
             </GlobalContext.Consumer>
         </>
