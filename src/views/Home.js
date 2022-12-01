c09d6a3777b557875914b72a130b3dc8833e53ac
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-12-01 18:05:10 -0500

    create group button added to home

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,6 +6,7 @@ import { GlobalContext } from '../lib/GlobalContext';
 import SearchIcon from '@mui/icons-material/Search';
 import { Link } from 'react-router-dom';
 import GroupsIcon from '@mui/icons-material/Groups';
+import AddIcon from '@mui/icons-material/Add';
 
 function GalleryLinks({shouldShow}) {
     if (!shouldShow) return <Typography variant="p" sx={{ mb: 2 }}><em>You're not currently looking for groups.</em></Typography>;
@@ -17,6 +18,9 @@ function GalleryLinks({shouldShow}) {
         <Button variant="contained" sx={{ mb: 2 }} startIcon={<SearchIcon />} component={Link} to="/gallery/students">
             Invite students
         </Button>
+        <Button color="secondary" variant="contained" sx={{ mb: 2 }} startIcon={<AddIcon />} component={Link} to="/group/new">
+            Start a group
+        </Button>
         <Divider sx={{ mb: 2 }} />
     </>;
 }
@@ -26,7 +30,7 @@ function Home() {
         <>
             <GlobalContext.Consumer>
                 {({ myProfile }) => <>
-                    <CreateGroupFab />
+                    {/* <CreateGroupFab /> */}
                     {/* <Typography variant="h4">You are currently <em>{myProfile.amLookingForGroups ? "looking" : "not looking"}</em> for groups.</Typography> */}
 
                     <Container maxWidth="md" sx={{ height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
